6e3deebb9c3b0275c6689abcacb4e41e2845ee44
AndrUsM <oksud22@gmail.com>
2020-01-28 12:08:28 +0200

    update headers constant

--- src/assets/js/dashboard/_partials/threads/send.js
+++ src/assets/js/dashboard/_partials/threads/send.js
@@ -1,23 +1,20 @@
-import {TYPES, FetchTemplate} from "../../../api_fetcher/index";
+import {TYPES, FetchTemplate, headers} from "../../../api_fetcher/index";
 import {config} from '../../../config';
 export class SendMessage{
     constructor(){
         this._id = window.localStorage.getItem("_id");
     }
     send(data){
-        console.log(data)
-        // new FetchTemplate().request('api/threads/messages', TYPES.post, data)
         fetch(`http://${config.domain}/api/threads/messages`, {
             method: TYPES.post,
-            headers: {
-                'x-access-token': localStorage.getItem('key'),
-                'Content-Type': 'application/json'
-            },
+            headers: headers.access,
             body: JSON.stringify(data)
         }).then(responce => {
-            console.log(responce.json())
-            if(responce.ok){
-                alert("OK");
+            console.log(data)
+            if(responce.status >= 200 && responce.status < 400){
+                alert("Succesfully send messege");
+            }else{
+                alert(`Occured error: ${responce.statusText}`)
             }
         })
     }
