0e7eab8be1e735e2d15723df342e79ed49ca3c00
AndrUsM <oksud22@gmail.com>
2020-01-28 12:09:05 +0200

    add default key value

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -2,6 +2,6 @@ export const config = {
     "host": "localhost",
     "port": 3000,
     "domain": "geekhub-frontend-js-9.herokuapp.com",
-    "key": window.localStorage.getItem("key") || "",
+    "key": window.localStorage.getItem("key") || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTE5YzIyM2E0MTk5YzAwMjI3NTI2OGEiLCJpYXQiOjE1Nzk2ODc4OTl9.M5q83O_nP6B8SbfNKOs3CaQTu4JaQcbr_MgDLSgqnTU",
     "interval": 5000
 }
